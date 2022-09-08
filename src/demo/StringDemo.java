656f7ec7762b608fc4dc12e3184615efc6ecd7d7
hongtayvy <hongtayvy@gmail.com>
2022-09-07 22:31:45 -0500

    chore: clean up stringDemo properly

--- src/demo/StringDemo.java
+++ src/demo/StringDemo.java
@@ -2,14 +2,19 @@ package demo;
 
 public class StringDemo {
     /**
-     * This method should print out the String variables.
-     *
-     * The result should show "Victor Yang"
+     * This method takes the String "Hello, World" and chops it up to only return "Hello".
      */
-    public static void runStringVariableProblem(){
-        String firstName = "Victor ";
-        String lastName = "Yang";
+    public static void runStringDemo(){
+        //1. This is the base string.
+        String helloWorld = "Hello, World";
 
-        System.out.println(firstName +  lastName);
+        //2. We find the index of the comma. This will return an index of 5.
+        int indexOfComma = helloWorld.indexOf(',');
+
+        //3. We chop up the string with using the method of substring and indexOfComma. This means we cut the word UP TO the comma not with the comma.
+        String hello = helloWorld.substring(indexOfComma);
+
+        //Print result.
+        System.out.println(hello);
     }
 }
