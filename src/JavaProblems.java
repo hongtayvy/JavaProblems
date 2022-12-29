67b97c2af40caeb31925e9ff1520f81c0ef6144d
Victor Yang <victoryang@Victors-MacBook-Pro.local>
2022-12-28 22:37:51 -0600

    chore: cleanup

--- src/JavaProblems.java
+++ src/JavaProblems.java
@@ -1,20 +1,9 @@
-import demo.ReturnCharacterDemo;
-import problem.EquationProblem;
-import problem.EquationReadFileProblem;
-import problem.test;
-
 import java.io.IOException;
 
-import static demo.ReturnCharacterDemo.runReturnCharacterDemo;
-import static problem.EquationProblem.runEquationProblem;
 import static problem.EquationReadFileProblem.runEquationReadFileProblem;
-import static problem.StringProblem.runStringProblem;
 
 public class JavaProblems {
-    public static void main(String[] args) {
-        EquationProblem.runEquationProblem();
-        ReturnCharacterDemo.runReturnCharacterDemo();
-        //can you explain why there is an unhandled exception in the following class and what that means? -S
-        EquationReadFileProblem.runEquationReadFileProblem();
+    public static void main(String[] args) throws IOException{
+        runEquationReadFileProblem();
     }
 }
