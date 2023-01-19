78e32e1f57e00f8d317188d0ab126be19959ee01
Shawn H <shherubin@gmail.com>
2023-01-18 22:19:21 -0600

    solved data manipulation problem

--- src/JavaProblems.java
+++ src/JavaProblems.java
@@ -1,9 +1,12 @@
+import problem.EquationReadFileProblem;
+
 import java.io.IOException;
 
 import static demo.StoreDataDemo.runStoreDataDemo;
 
 public class JavaProblems {
     public static void main(String[] args) throws IOException {
-        runStoreDataDemo();
+        //runStoreDataDemo();
+        EquationReadFileProblem.runEquationReadFileProblem();
     }
 }
