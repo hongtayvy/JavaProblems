fa88b2d5d4fbf9a79bb6cffcce6067e2b7ea1bee
Victor Yang <victoryang@Victors-M1Pro.local>
2023-08-15 09:46:39 -0500

    chore: added theo's problems

--- src/basics/theo/problem/EquationReadFileProblem.java
+++ src/basics/theo/problem/EquationReadFileProblem.java
@@ -23,9 +23,6 @@ public class EquationReadFileProblem {
      * @throws IOException
      */
     public static void runEquationReadFileProblem() throws IOException {
-        String shawnsEquation = BasicFileReader.readFile("src/data/equation.txt");
-        String[] cleanData = constructData(shawnsEquation);
-        checkData(cleanData);
     }
 
     /**
@@ -45,64 +42,9 @@ public class EquationReadFileProblem {
 
     private static void checkData(String[] cleanData) {
 
-        for (int i = 0; i < cleanData.length; i++) {
-            System.out.println(cleanData[i]);
-
-            int leftHandOfOperation = cleanData[i].indexOf(' ');
-            int operator = cleanData[i].indexOf('+');
-            int rightHandOfOperation = cleanData[i].indexOf('=');
-            int proposedAnswer = cleanData[i].lastIndexOf(' ');
-
-            System.out.println("Left hand operation is " + leftHandOfOperation);
-            System.out.println(operator);
-            System.out.println(rightHandOfOperation);
-            System.out.println(proposedAnswer);
-
-            String modifiedFirstNumber = cleanData[i].substring(leftHandOfOperation + 1, operator);
-            String modifiedOperator = cleanData[i].substring(operator, operator + 1);
-            String modifiedSecondNumber = cleanData[i].substring(operator + 2, rightHandOfOperation);
-            String modifiedAnswer = cleanData[i].substring(rightHandOfOperation + 2);
-
-            System.out.println("Here is our modified answer" + modifiedAnswer);
-            String answer = String.valueOf(Integer.parseInt(modifiedFirstNumber.trim()) + Integer.parseInt(modifiedSecondNumber.trim()));
-
-            if (answer.equals(modifiedAnswer.trim())) {
-                System.out.println("You have correctly answered the question, congratz.");
-            } else {
-                System.out.println("You have incorrectly answered, try again");
-            }
-        }
     }
 
     private static String[] constructData(String equationData){
-
-        int endFirstEquation = equationData.indexOf("\n");
-        int endSecondEquation = equationData.lastIndexOf("\n");
-
-        String firstEquationFull = equationData.substring(0, endFirstEquation);
-        String secondEquationFull = equationData.substring(endFirstEquation + 1, endSecondEquation);
-        String thirdEquationFull = equationData.substring(endSecondEquation + 1);
-
-
-        String [] shawnsArray = new String[3];
-
-        for(int i = 0; i < shawnsArray.length; i++ ) {
-            switch (i) {
-                case 0:
-                    shawnsArray[i] = firstEquationFull;
-                    break;
-                case 1:
-                    shawnsArray[i] = secondEquationFull;
-                    break;
-                case 2:
-                    shawnsArray[i] = thirdEquationFull;
-                    break;
-            }
-        }
-        for(int i = 0; i < shawnsArray.length; i++ ) {
-            System.out.println("final array" + shawnsArray[i]);
-        }
-    return shawnsArray;
     }
 
 }
